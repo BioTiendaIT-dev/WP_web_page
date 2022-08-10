6af236e990bd855adabe0c434e4e7508b319291a
Luan E <luan.erazo@skyrocketlab.space>
2022-08-10 09:45:10 -0500

    Cambios generales en Tawilwind

--- wp-content/themes/bioTienda/tailwind.config.js
+++ wp-content/themes/bioTienda/tailwind.config.js
@@ -2,7 +2,31 @@
 module.exports = {
   content: ["./*.php", "./woocommerce/**/*.php"],
   theme: {
-    extend: {},
+    fontFamily: {
+      raleway: ["Raleway", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        white: {
+          primary: "#ffffff",
+          secondary: "#D78D5B00",
+        },
+        gray: {
+          100: "#E6E6E6",
+          200: "#CFCFCF",
+          300: "#999999",
+          400: "#7C7C7C",
+        },
+        green: {
+          DEFAULT: "#AAC940",
+          dark: "#086175",
+        },
+        brown: {
+          100: "#92430C87",
+          200: "#8D501A",
+        },
+      },
+    },
   },
   plugins: [],
 };
