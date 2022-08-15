ae2ac55533483acebabfaefa863d9afc0a7e6d3d
Luan E <luan.erazo@skyrocketlab.space>
2022-08-15 18:32:24 -0500

    arreglo de mobile home, agregados logica para pagina tienda

--- wp-content/themes/bioTienda/footer.php
+++ wp-content/themes/bioTienda/footer.php
@@ -1,3 +1,6 @@
+<footer>
+    <h2>un footer</h2>
+</footer>
 <?php wp_footer() ?>
 </body>
 
---
+++ wp-content/themes/bioTienda/sidebar.php
@@ -0,0 +1,5 @@
+<section id="categories" class="col-span-1"><?php _e('Categories:'); ?>
+    <ul>
+        <?php wp_list_categories(); ?>
+    </ul>
+</section>
