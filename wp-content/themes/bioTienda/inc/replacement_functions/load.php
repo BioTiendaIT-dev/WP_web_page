6781353c8006e4dcd221e04f191a349f0df6e2d6
Luan E <luan.erazo@skyrocketlab.space>
2023-10-03 18:00:43 -0500

    agregando swiper como visor del producto individual, falta implementacion swiper principal y thumbnail

--- wp-content/themes/bioTienda/inc/replacement_functions/load.php
+++ wp-content/themes/bioTienda/inc/replacement_functions/load.php
@@ -1,5 +1,6 @@
 <?php
 require 'woocommerce_loop_intro.php';
+require 'woocommerce_single-product.php';
 // ------------------------------ este siempre debe de ser el ultimo
 require 'template_hooks.php';
 ?>
